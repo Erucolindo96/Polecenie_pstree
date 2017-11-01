3f9988c0b1a5b056f3d7a7e72c1c54c6c8616d6b
Erucolindo <erucolindo96@gmail.com>
2017-11-01 21:48:19 +0100

    Chyba userzy dzialaja. Nalezy sformatowac kod, ew poprawic wykrywanie bledow skladniowych przy wpisywaniu parametrow skryptu

--- pstree.pl
+++ pstree.pl
@@ -60,22 +60,22 @@ sub readParameters
 		$is_param{"-help"} = 1;	
 	}
 
-	if ($all_args =~ / -u ([A-z]+)/)
+	if ($all_args =~ / -u (\S+)/)
 	{
 		$is_param{"-u"} = 1;
 		$args_param{"-u"} = $1;
 	}
-	elsif ($all_args !~ / -u ([A-z]+)/ and $all_args =~ / -u / )#jesli jest -u a nie ma podanego pliku
+	elsif ($all_args !~ / -u (\S+)/ and $all_args =~ / -u / )#jesli jest -u a nie ma podanego pliku
 	{
 		$must_close = 1;
 	}
 
-	if ($all_args =~ / -png ([\D]+)/)
+	if ($all_args =~ / -png (\S+)/)
 	{
 		$is_param{"-png"} = 1;
 		$args_param{"-png"} = $1;
 	}
-	elsif ($all_args !~ / -png ([\D]+)/ and $all_args =~ / -png / )#jesli jest -u a nie ma podanego pliku
+	elsif ($all_args !~ / -png (\S+)/ and $all_args =~ / -png / )#jesli jest -u a nie ma podanego pliku
 	{
 		$must_close = 1;
 	}
@@ -86,17 +86,17 @@ sub readParameters
 
 sub readProcessData
 {
-	open(INP, "ps -e --format=\"pid ppid user comm\" | "); 
+	open(INP, "ps -e --format=\"pid ppid comm user\" | "); 
 	my $line = "";
 	while(<INP>)
 	{
 		$line = $_;
 		chomp($line);
-		$line =~ /[ ]*(\d+)[ ]+(\d+)[ ]+(\D+)[ ]+(\D+)/;
+		$line =~ /[ ]*(\d+)[ ]+(\d+)[ ]+(\S+)[ ]+(\S+)[ ]*/;
 		
 		$pid_to_ppid{$1} = $2;
-		$pid_to_name{$1} = $4;
-		$pid_to_user{$1} = $3;
+		$pid_to_name{$1} = $3;
+		$pid_to_user{$1} = $4;
 	}
 	&initTreeVariable();	
 	close(INP);
@@ -220,6 +220,31 @@ sub howManyLinesNeed
 	return $lines;
 }
 
+
+# Zwraca informacje, czy dany PID ma dzieci, ktore zostaly wywolane przed usera danego jako argument(lub sam nalezy do tego usera)
+# Argument 1 - PID procesu ktorego pytamy,  2 - nazwa usera
+# Zwracana wartość - 1 jeśli ma dzieci nalezace do usera, 0 jesli nie ma
+#
+#
+
+sub haveChildUser
+{
+	my $my_pid = $_[0];
+	my $user_name = $_[1];
+	return 1 if($pid_to_user{$my_pid} eq $user_name);
+	my @children = &findChildren($my_pid);
+	foreach $child (@children)
+	{
+		if(&haveChildUser($child, $user_name) == 1)
+		{
+			return 1;
+		}
+	}
+	return 0;
+
+}
+
+
 #zwraca string zawierajacy tyle spacji ile podano jako argument
 sub generateSpace
 {
@@ -231,7 +256,9 @@ sub generateSpace
 	return $string;	
 }
 
-
+#Rekurencyjnie generuje drzewo od PIDu z ktorego mamy zaczac, oraz wiersza i kolumny w ktorej ma pojawic sie pierwszy proces(korzen drzewa
+#
+#)
 sub generateNextNode	#argument 1 - wiersz w ktorym zaczyna pisac swoje pierwsze dziecko
 {			#argument 2 - kolumna w ktorej piszemy 
 			#argument 3 - swoj PID
@@ -255,7 +282,7 @@ sub generateNextNode	#argument 1 - wiersz w ktorym zaczyna pisac swoje pierwsze 
 		$act_col = $act_col + length("($my_pid)");
 	}
 	#jesli masz dzieci - dopisz separator poziomy
-	if(scalar(@children) != 0)
+	if(scalar(@children) != 0 )
 	{
 		$tree[$act_line] = $tree[$act_line].$separ_hor;
 		$act_col = $act_col + $sep_size;
@@ -268,7 +295,8 @@ sub generateNextNode	#argument 1 - wiersz w ktorym zaczyna pisac swoje pierwsze 
 	my $lines_needed = 0;
 	foreach $child (@children)
 	{
-		$lines_needed =$lines_needed +  &howManyLinesNeed($child);
+		
+			$lines_needed =$lines_needed +  &howManyLinesNeed($child);
 	}
 	$lines_needed--; #bo pierwszom liniom jest nasz napis
 	$space = &generateSpace(length($my_name)+1); #+1 bo przez nasza nazwa dalismy "_"
@@ -279,15 +307,100 @@ sub generateNextNode	#argument 1 - wiersz w ktorym zaczyna pisac swoje pierwsze 
 	#wywowaj rekursywnie fcje generateNextNode
 	foreach $child(@children)
 	{
-		$act_line = &generateNextNode($act_line, $act_col, $child);
-		$act_line++;
+			$act_line = &generateNextNode($act_line, $act_col, $child);
+			$act_line++;
+	}
+	return $begin_line;
+}
+#Robi to co generateNextNode, ale bierze pod uwagę rowniez usera podanego jako parametr - i tworzy tylko te dzieci, ktore zawieraja gdzies dalej procesy
+#powolane do zycia przez usera podanego jako parametr
+
+sub generateNextNodeUser	#argument 1 - wiersz w ktorym zaczyna pisac swoje pierwsze dziecko
+{				#argument 2 - kolumna w ktorej piszemy 
+				#argument 3 - swoj PID
+				#argument 4 - nazwa usera ktorego procesy mamy wygenerowac
+				#wartosc zwracana - wiersz w ktorym piszemy kolejne dziecko
+
+	my $act_line = $_[0], $act_col = $_[1], $begin_col = $_[1], $begin_line = $_[0];
+	my $my_pid = $_[2];
+	my $user_to_find = $_[3];
+
+	my $my_name = $pid_to_name{$my_pid};
+	my @children = &findChildren($my_pid);
+	
+	my $separ_hor = "__", $sep_vert = " |";
+	my $sep_size = 2;
+
+	#wpisz "_"  i  samego siebie jesli zawierasz dzieci nalezace do danego usera
+	if(&haveChildUser($my_pid, $user_to_find) == 1 )
+	{
+		$tree[$act_line] = $tree[$act_line]."_".$my_name;
+		$act_col = $act_col + 1 + length($my_name);
+	
+		#jesli masz wpisywac rozwniez PIDY - wpisz go
+		if($is_param{"-p"} == 1)
+		{
+			$tree[$act_line] = $tree[$act_line]."($my_pid)";
+			$act_col = $act_col + length("($my_pid)");
+		}
+		#jesli nalezymy do danego usera - piszemy to
+		if($pid_to_user{$my_pid} eq $user_to_find)
+		{
+			$tree[$act_line] = $tree[$act_line]."(U)";
+			$act_col = $act_col + length("(U)");
+
+		}
+	}
+
+	#jesli masz dzieci ktore beda zawierac jakies procesy szukanego usera - dopisz separator poziomy
+	if(scalar(@children) != 0 and &haveChildUser($my_pid, $user_to_find)==1 )
+	{
+		$tree[$act_line] = $tree[$act_line].$separ_hor;
+		$act_col = $act_col + $sep_size;
+	}
+
+	#dla kazdego dziecka oblicz ile zajmie linijek
+	#w tylu wierszach - 1 (bo jedna juz jest wypisana) ponizej aktualnego
+	#dodaj tyle spacji ile zajmuje $my_name(od kolumny poczatkowej)
+	#oraz separator pionowy
+	my $lines_needed = 0;
+	foreach $child (@children)
+	{
+			if(&haveChildUser($child, $user_to_find) == 1 )
+			{
+				$lines_needed =$lines_needed +  &howManyLinesNeed($child);
+			}
+	}
+	$lines_needed--; #bo pierwszom liniom jest nasz napis
+	$space = &generateSpace(length($my_name)+1); #+1 bo przez nasza nazwa dalismy "_"
+	for($i = 1; $i <=$lines_needed; $i++)
+	{
+		$tree[$act_line+$i] = $tree[$act_line+$i].$space.$sep_vert;
+	}
+	#wywowaj rekursywnie fcje generateNextNode
+	foreach $child(@children)
+	{	
+		if(&haveChildUser($child, $user_to_find) == 1 )
+		{
+			$act_line = &generateNextNodeUser($act_line, $act_col, $child, $user_to_find);
+			$act_line++;
+		}
 	}
 	return $begin_line;
 }
 
+
+
 sub generateTree #robi to co generateNextNode, ale zaczyna wypisywac
 {
-	&generateNextNode(0,0,1);
+	if($is_param{"-u"} == 0 )
+	{
+		&generateNextNode(0,0,1);
+	}
+	else
+	{
+		&generateNextNodeUser(0,0,1, $args_param{"-u"} );
+	}
 	$tree[0] = substr($tree[0],1, length($tree[0]) ); 
 }
 
@@ -318,35 +431,16 @@ sub printTreeToImage
 sub printTreeToPngFile
 {
 
-	if($is_param{"-u"} == 0 )#wypisz cale drzewo
-	{
-		&generateTree();	
-		&printTreeToImage();	
-	}
-	else#wypisz tylko procesy danego usera
-	{
-		&generateTree();	
-		&printTreeToImage();	
-	}
+	&generateTree();	
+	&printTreeToImage();	
 	$fname = $args_param{"-png"};
 	print "Zapisano do pliku: $fname\n";
 
 }
 sub printTreeToConsole
 {
-	if($is_param{"-u"} == 0 )#wypisz cale drzewo
-	{
-		&generateTree();	
-		&printTreeVariable();
-		
-	}
-	else#wypisz tylko procesy danego usera
-	{
-		&generateTree();	
-		&printTreeVariable();
-	}
-
-
+	&generateTree();	
+	&printTreeVariable();
 }
 
 #
@@ -365,7 +459,7 @@ sub chooseOperationAndExecute
 	}
 	else #nie wybrano wyswietlania pomocy - trzeba w jakis sposob wyswietlic drzewo
 	{
-		&findUserProcesses if($is_param{"-u"} == 1);
+		#&findUserProcesses if($is_param{"-u"} == 1);
 
 		if($is_param{"-png"} == 1)
 		{
@@ -378,8 +472,16 @@ sub chooseOperationAndExecute
 	}
 }
 
-
-
+#argument - pid
+sub test
+{
+	my $pid = $_[0];
+	my $user = $args_param{"-u"};
+	print "Proces o nazwie $pid_to_name{$pid}";
+	print " Czy ma dzieci o uzytkowniku root? :";
+	print &haveChildUser($pid, $user), "\n";
+	print "nazwa szukanego usera : |", $user, "|\n"  
+}
 
 
 sub main_f
@@ -394,7 +496,6 @@ sub main_f
 	}
 	else
 	{
-
 		&chooseOperationAndExecute();
 	}
 
